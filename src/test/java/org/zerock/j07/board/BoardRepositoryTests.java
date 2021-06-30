cf3efed6ae40b0c5b74df81fedaa1aec8e31d86d
sukhyunil <sukhyunil@naver.com>
2021-06-30 20:00:17 +0900

    Initial commit

---
+++ src/test/java/org/zerock/j07/board/BoardRepositoryTests.java
@@ -0,0 +1,38 @@
+package org.zerock.j07.board;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.zerock.j07.board.entity.Board;
+import org.zerock.j07.board.repository.BoardRepository;
+
+import java.util.stream.IntStream;
+
+@ActiveProfiles("dev")
+@SpringBootTest
+@Log4j2
+public class BoardRepositoryTests {
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    @Test
+    public void insertDummies(){
+
+        IntStream.rangeClosed(1,200).forEach(i ->{
+            Board board = Board.builder()
+                    .title("제목..")
+                    .content("content....")
+                    .writer("user"+i)
+                    .build();
+            boardRepository.save(board);
+
+            log.info(board);
+        });//end loop
+
+    }
+
+
+}
